2e0c232862ae62ccb119108c7183b0bcbb6c5d0e
Ionut <ionut.pisla@vanroey.be>
2017-11-06 12:27:14 +0200

    Fixed php 7.1 cast for 2.15 branch

--- classes/phing/Phing.php
+++ classes/phing/Phing.php
@@ -1628,14 +1628,17 @@ class Phing
         $val = trim($val);
         $last = strtolower($val[strlen($val) - 1]);
 
-        switch ($last) {
-            // The 'G' modifier is available since PHP 5.1.0
-            case 'g':
-                $val *= 1024;
-            case 'm':
-                $val *= 1024;
-            case 'k':
-                $val *= 1024;
+        if (!is_numeric($last)) {
+            $val = (int) substr($val, 0, strlen($val) - 1);
+            switch ($last) {
+                // The 'G' modifier is available since PHP 5.1.0
+                case 'g':
+                    $val *= 1024;
+                case 'm':
+                    $val *= 1024;
+                case 'k':
+                    $val *= 1024;
+            }
         }
 
         return $val;
